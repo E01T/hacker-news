63984945df115244de26459c0dfbb550b5fec1fa
Efthyvoulos Tsouderos <etsouderos@gmail.com>
2020-09-01 16:11:56 +0300

    Routes & Tabs added

---
+++ src/pages/Tabs/Trending.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { IonPage, IonHeader, IonToolbar, IonTitle } from "@ionic/react";
+
+export const Trending = () => {
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Trending</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+    </IonPage>
+  );
+};
+
+export default Trending;
