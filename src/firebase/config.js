084d1e28237a3d1393034278e1596f8ba93f3f10
E01T <etsouderos@gmail.com>
2020-09-11 12:11:07 +0300

    Update config.js

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,11 +1,12 @@
+// Add your firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC4eawvNd9qkVI6D8DWvMNpwnCTs9ejqxQ",
-  authDomain: "hackernews-8b245.firebaseapp.com",
-  databaseURL: "https://hackernews-8b245.firebaseio.com",
-  projectId: "hackernews-8b245",
-  storageBucket: "hackernews-8b245.appspot.com",
-  messagingSenderId: "435769741800",
-  appId: "1:435769741800:web:053613c8826c485503145d",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 export default firebaseConfig;
